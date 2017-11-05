09c30326cf5dba53948c0fba2b4f0bfa3e9e2b28
Leping08 <dereks008@gmail.com>
2017-11-05 09:35:15 -0500

    Add title and remove add button from groups page

--- resources/views/layouts/app-uikit.blade.php
+++ resources/views/layouts/app-uikit.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>The Swim School</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- uikit stuff -->
