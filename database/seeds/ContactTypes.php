1970b54e534da3b2698034938707acac34287ce1
Leping08 <dereks008@gmail.com>
2017-11-10 07:20:34 -0500

    Add contact types table seeding

---
+++ database/seeds/ContactTypes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ContactTypes extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('contact_types')->insert([
+            'name' => 'Contact Us',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime
+        ]);
+
+        DB::table('contact_types')->insert([
+            'name' => 'Request A Lifeguard',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime
+        ]);
+
+        DB::table('contact_types')->insert([
+            'name' => 'CPR and First Aid Training',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime
+        ]);
+    }
+}
