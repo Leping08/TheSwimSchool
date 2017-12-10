a25fe0b2996d91a1729f3b6701e8a48b8103b2d8
Leping08 <dereks008@gmail.com>
2017-12-10 16:47:17 -0500

    Update swimmer contoller to send emails for new lesson signup

--- app/Http/Controllers/SwimmerController.php
+++ app/Http/Controllers/SwimmerController.php
@@ -85,13 +85,13 @@ class SwimmerController extends Controller
         //If the user is using a card for payment, send them to the card view with the user id.
         if(request('payment') === 'card'){
             //TODO: add signup email to the que
-            //Mail::to($newSwimmer->email)->send(new SignUp($lesson));
+            Mail::to($newSwimmer->email)->send(new SignUp($lesson));
             return view('swimmers.cardCheckout', compact('newSwimmer', 'lesson'));
             //If they are paying in person, redirect them to the class they signed up for with success alert.
         }elseif(request('payment') === 'check'){
             //TODO: add signup email to the que
-            SignupEmail::dispatch($lesson, $newSwimmer->email);
-            //Mail::to($newSwimmer->email)->send(new SignUp($lesson));
+            //SignupEmail::dispatch($lesson, $newSwimmer->email);
+            Mail::to($newSwimmer->email)->send(new SignUp($lesson));
             $request->session()->flash('success', 'You are all signed up! First lesson is '.$lesson->class_start_date->toFormattedDateString().' at '.$lesson->class_start_time->format('H:i A').'. Be sure to bring cash or check for $'.$lesson->price.' to the first lesson.');
             return redirect('lessons/'.$lesson->class_type);
         }else{
