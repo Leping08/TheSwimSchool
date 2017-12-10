05b818cdc02dac439915e8afa20c4712dc3cf839
Leping08 <dereks008@gmail.com>
2017-12-10 12:26:38 -0500

    Add what to bring to the lesson sign up email

--- resources/views/email/lessonSignUp.blade.php
+++ resources/views/email/lessonSignUp.blade.php
@@ -5,13 +5,20 @@ Thanks for signing up for {{$lesson->group->type}} swim lessons.
 
 @component('mail::panel')
 ### Time
-The first class is {{$lesson->class_start_date->format('l F jS')}} at {{$lesson->class_start_time->format('H:i A')}}.
+The first class is {{$lesson->class_start_date->format('l F jS')}}, {{$lesson->class_start_time->format('H:i A')}} - {{$lesson->class_start_end->format('H:i A')}}.
 @endcomponent
 
 @component('mail::panel')
-    ### Place
-    {{$lesson->location->street}},
-    {{$lesson->location->city}}, {{$lesson->location->state}} {{$lesson->location->zip}}
+### Place
+{{$lesson->location->street}},
+{{$lesson->location->city}}, {{$lesson->location->state}} {{$lesson->location->zip}}
+@endcomponent
+
+@component('mail::panel')
+### What To Bring
+* Swimwear
+* Towel
+* Goggles
 @endcomponent
 
 @component('mail::button', ['url' => ''])
