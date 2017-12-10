2292a6ba0234b7927c15d10b6bdfcea95cb5b6ba
Leping08 <dereks008@gmail.com>
2017-12-10 12:31:08 -0500

    Fix class end time in sign up emai

--- resources/views/email/lessonSignUp.blade.php
+++ resources/views/email/lessonSignUp.blade.php
@@ -5,7 +5,7 @@ Thanks for signing up for {{$lesson->group->type}} swim lessons.
 
 @component('mail::panel')
 ### Time
-The first class is {{$lesson->class_start_date->format('l F jS')}}, {{$lesson->class_start_time->format('H:i A')}} - {{$lesson->class_start_end->format('H:i A')}}.
+The first class is {{$lesson->class_start_date->format('l F jS')}}, {{$lesson->class_start_time->format('H:i A')}} - {{$lesson->class_end_time->format('H:i A')}}.
 @endcomponent
 
 @component('mail::panel')
