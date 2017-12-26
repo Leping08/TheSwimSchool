812adf3e9c03ea136ae3141dd191477258482aa4
Leping08 <dereks008@gmail.com>
2017-12-26 18:57:00 -0500

    Use smaller icons for email footer

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -21,15 +21,17 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            <div style="padding-top:10px; text-align:center !important;">
-                <a href="https://www.facebook.com/DealersUnited/" target="blank"><img style="padding: 10px;" src="{{asset('/img/phone-in-talk.png')}}"></a>
-                <a href="mailto:theswimschoolfl@gmail.com"><img style="padding: 10px;" src="{{asset('/img/email.png')}}"></a>
-                <a href="https://www.facebook.com/theswimschoolfl/" target="blank"><img style="padding: 10px;" src="{{asset('/img/facebook-box.png')}}"></a>
-            </div>
+            <div>
+                <div style="padding-top:10px; text-align:center !important;">
+                    <a href="tel:1-941-773-1424" target="blank"><img style="padding: 10px;" src="{{asset('/img/phone.png')}}"></a>
+                    <a href="mailto:theswimschoolfl@gmail.com"><img style="padding: 10px;" src="{{asset('/img/email.png')}}"></a>
+                    <a href="https://www.facebook.com/theswimschoolfl/" target="blank"><img style="padding: 10px;" src="{{asset('/img/facebook-box.png')}}"></a>
+                </div>
 
-            <p>
-                &copy; {{ date('Y') }} The Swim School. All rights reserved.<br>
-            </p>
+                <p>
+                    &copy; {{ date('Y') }} The Swim School. All rights reserved.
+                </p>
+            </div>
         @endcomponent
     @endslot
 @endcomponent
