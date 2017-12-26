60c916016d32c06b6f66a1d24b909609171eca65
Leping08 <dereks008@gmail.com>
2017-12-26 18:45:05 -0500

    Add email, phone, and facebook icons to footer of email template

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -21,9 +21,13 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
+            <div style="padding-top:10px; text-align:center !important;">
+                <a href="https://www.facebook.com/DealersUnited/" target="blank"><img style="padding: 10px;" src="{{asset('/img/phone-in-talk.png')}}"></a>
+                <a href="mailto:theswimschoolfl@gmail.com"><img style="padding: 10px;" src="{{asset('/img/email.png')}}"></a>
+                <a href="https://www.facebook.com/theswimschoolfl/" target="blank"><img style="padding: 10px;" src="{{asset('/img/facebook-box.png')}}"></a>
+            </div>
 
             <p>
-                <!-- TODO: add Facebook, phone, email icons with links -->
                 &copy; {{ date('Y') }} The Swim School. All rights reserved.<br>
             </p>
         @endcomponent
