7959dfc0b333084f67b61cf22593a970e80ed86b
Leping08 <dereks008@gmail.com>
2017-12-26 19:01:23 -0500

    Update footer icon image names

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -23,9 +23,9 @@
         @component('mail::footer')
             <div>
                 <div style="padding-top:10px; text-align:center !important;">
-                    <a href="tel:1-941-773-1424" target="blank"><img style="padding: 10px;" src="{{asset('/img/phone.png')}}"></a>
-                    <a href="mailto:theswimschoolfl@gmail.com"><img style="padding: 10px;" src="{{asset('/img/email.png')}}"></a>
-                    <a href="https://www.facebook.com/theswimschoolfl/" target="blank"><img style="padding: 10px;" src="{{asset('/img/facebook-box.png')}}"></a>
+                    <a href="tel:1-941-773-1424" target="blank"><img style="padding: 10px;" src="{{asset('phone-24x24.png')}}"></a>
+                    <a href="mailto:theswimschoolfl@gmail.com"><img style="padding: 10px;" src="{{asset('email-24x24.png')}}"></a>
+                    <a href="https://www.facebook.com/theswimschoolfl/" target="blank"><img style="padding: 10px;" src="{{asset('facebook-box-24x24.png')}}"></a>
                 </div>
 
                 <p>
