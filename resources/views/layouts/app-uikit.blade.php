30700454a16bdc49cf6c1641208e38d5ba6905dc
Leping08 <dereks008@gmail.com>
2018-02-16 16:53:48 -0500

    Add GTM to the layout page

--- resources/views/layouts/app-uikit.blade.php
+++ resources/views/layouts/app-uikit.blade.php
@@ -4,6 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5LBC84D');
+    </script>
     @yield('seo')
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -16,7 +24,8 @@
     </script>
 </head>
 <body>
-    
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LBC84D" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     @include('nav')
 
 
