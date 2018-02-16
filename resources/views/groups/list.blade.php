3a0cc7d660d91839d5fc695cf2825540c77ebee8
Leping08 <dereks008@gmail.com>
2018-02-16 11:00:21 -0500

    Add no groups available to groups list page

--- resources/views/groups/list.blade.php
+++ resources/views/groups/list.blade.php
@@ -7,26 +7,32 @@
 @section('content')
     <div class="uk-section-default uk-section-overlap uk-section">
         <div class="uk-container">
-            @foreach($groups as $group)
-                <div class="uk-margin-top">
-                    <div class="uk-card uk-card-default uk-width-1-1@s" uk-scrollspy="cls: uk-animation-slide-bottom; delay: 250">
-                        <div class="uk-card-header">
-                            <div class="uk-grid-small uk-flex-middle" uk-grid>
-                                <div class="uk-width-expand">
-                                    <div class="uk-card-badge uk-label">{{$group->ages}}</div>
-                                    <h3 class="uk-card-title f-24 uk-heading-bullet">{{$group->type}}</h3>
+            @if(count($groups))
+                @foreach($groups as $group)
+                    <div class="uk-margin-top">
+                        <div class="uk-card uk-card-default uk-width-1-1@s" uk-scrollspy="cls: uk-animation-slide-bottom; delay: 250">
+                            <div class="uk-card-header">
+                                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                                    <div class="uk-width-expand">
+                                        <div class="uk-card-badge uk-label">{{$group->ages}}</div>
+                                        <h3 class="uk-card-title f-24 uk-heading-bullet">{{$group->type}}</h3>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="uk-card-body">
-                            <p>{{$group->description}}</p>
-                        </div>
-                        <div class="uk-card-footer">
-                            <a href="/lessons/{{{$group->type}}}" class="uk-button uk-button-primary">Find Classes</a>
+                            <div class="uk-card-body">
+                                <p>{{$group->description}}</p>
+                            </div>
+                            <div class="uk-card-footer">
+                                <a href="/lessons/{{{$group->type}}}" class="uk-button uk-button-primary">Find Classes</a>
+                            </div>
                         </div>
                     </div>
+                @endforeach
+            @else
+                <div class="uk-card-body">
+                    No groups available.
                 </div>
-            @endforeach
+            @endif
         </div>
     </div>
 @endsection
