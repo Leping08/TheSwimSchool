05a91a038b6b5ccc5e2f977e9901d1ec7335f776
Leping08 <dereks008@gmail.com>
2018-03-13 20:23:36 -0400

    Update private lessons page

--- resources/views/pages/private-semi-private-lessons.blade.php
+++ resources/views/pages/private-semi-private-lessons.blade.php
@@ -15,7 +15,9 @@
             <div class="uk-grid-margin uk-grid" uk-grid="">
                 <div class="uk-width-3-4@m uk-first-column">
                     <div class="uk-margin uk-dropcap">
-                        Whether you are a beginner swimmer, a child preparing to join a <a title="Parrish Swim Team" href="/swim-team">swim team</a>, or a triathlete looking to improve your technique, private and semi-private swim lessons can be customized to your specific needs to help you achieve your goals quickly. Private lessons are conducted one-on-one with an instructor but if you have siblings or friends close in age and ability level who would like to receive swim instruction together, you can also request semi-private lessons. These lessons can be purchased monthly in packages of four (4) or eight (8) lessons at your pool or our Harrison Ranch location.
+                        Whether you are a beginner swimmer, a child preparing to join a <a title="Parrish Swim Team" href="/swim-team">swim team</a>, or a triathlete
+                        looking to improve your technique, private and semi-private swim lessons can be customized to your specific needs to help you achieve your goals quickly.
+                        These lessons can be purchased monthly in packages of four (4) or eight (8) lessons at your pool or our Harrison Ranch location.
                     </div>
                 </div>
                 <div class="uk-width-expand@m">
@@ -30,6 +32,7 @@
             <div class="uk-grid-margin uk-grid" uk-grid="">
                 <div class="uk-width-4-4@m uk-first-column">
                     <h2 class="uk-heading-bullet">Private Lesson Package Options</h2>
+                    <p class="uk-text-meta">Private lessons are conducted one-on-one with an instructor.</p>
                     <div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
                         <div>
                             <div class="uk-card uk-card-default uk-card-body">
@@ -54,6 +57,7 @@
 
                 <div class="uk-width-4-4@m uk-first-column">
                     <h2 class="uk-heading-bullet">Semi-Private Lesson Package Options</h2>
+                    <p class="uk-text-meta">Semi-Private lessons can be requested if you have siblings or friends close in age and ability level who would like to receive swim instruction together</p>
                     <div class="uk-child-width-1-2@m uk-grid-small uk-grid-match" uk-grid>
                         <div>
                             <div class="uk-card uk-card-default uk-card-body">
