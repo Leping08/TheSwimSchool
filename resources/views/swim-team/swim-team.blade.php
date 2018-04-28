bb813c77d26a5eaf6bb69e810adab35a47462050
Leping08 <dereks008@gmail.com>
2018-04-27 22:01:18 -0400

    Update swim team page

--- resources/views/swim-team/swim-team.blade.php
+++ resources/views/swim-team/swim-team.blade.php
@@ -15,26 +15,27 @@
             <div class="uk-flex-middle uk-grid-margin uk-grid" uk-grid="">
                 <div class="uk-width-expand@m uk-first-column">
                     <div class="uk-dropcap">
-                        <p>Bring your suit, goggles & towel and come join our team! The North River Swim Team is holding tryouts at the River Wilderness Country Club. Registration for pre-season tryouts opens April 1st, 2018.</p>
+                        <p>Bring your suit, goggles & towel and come join our team! The North River Swim Team practices at the River Wilderness Country Club and competes in a seasonal, developmental swim league that runs May 1st - August 31st. Tryouts are held every Tuesday at 6:30PM. There is no fee to tryout for the swim team.</p>
+                        {{--<p>Bring your suit, goggles & towel and come join our team! The North River Swim Team is holding tryouts at the River Wilderness Country Club. Registration for pre-season tryouts opens April 1st, 2018.</p>--}}
                     </div>
-                    <div>
-                        <h3 class="uk-heading-line"><span>2018 Pre-Season Tryout Dates</span></h3>
-                        <ul class="uk-list uk-list-bullet">
-                            <li>Wed. 4/18 6:30PM</li>
-                            <li>Thurs. 4/19 6:30PM</li>
-                            <li>Sat. 4/21 10:00AM</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p><strong>There is no fee to tryout for the swim team.</strong></p>
-                    </div>
+                    {{--<div>--}}
+                        {{--<h3 class="uk-heading-line"><span>2018 Pre-Season Tryout Dates</span></h3>--}}
+                        {{--<ul class="uk-list uk-list-bullet">--}}
+                            {{--<li>Wed. 4/18 6:30PM</li>--}}
+                            {{--<li>Thurs. 4/19 6:30PM</li>--}}
+                            {{--<li>Sat. 4/21 10:00AM</li>--}}
+                        {{--</ul>--}}
+                    {{--</div>--}}
+                    {{--<div>--}}
+                        {{--<p><strong>There is no fee to tryout for the swim team.</strong></p>--}}
+                    {{--</div>--}}
                     <div>
                         <p>The North River Swim Team practices at the <a title="Parrish swim team" target="_blank" href="http://www.riverwildernesscc.com/">River Wilderness Country Club</a> and competes in the Suncoast Swim League, a seasonal developmental league that runs May 1st - August 31st.</p>
                     </div>
-                    <div>
-                        <a class="uk-button uk-button-primary" href="/swim-team/tryouts">Sign Up for Tryouts</a>
+                    {{--<div>--}}
+                        {{--<a class="uk-button uk-button-primary" href="/swim-team/tryouts">Sign Up for Tryouts</a>--}}
                         {{--<a class="uk-button uk-button-default" disabled>Registration Coming Soon</a>--}}
-                    </div>
+                    {{--</div>--}}
                 </div>
                 <div class="uk-width-expand@m">
                     <div class="uk-margin">
