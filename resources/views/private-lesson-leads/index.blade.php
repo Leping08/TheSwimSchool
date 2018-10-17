f36f51a0c6fe279d8a6ab1cdc5505d9d781a60d8
Leping08 <dereks008@gmail.com>
2018-10-16 21:56:54 -0400

    Remove private lesson request and add alert box

--- resources/views/private-lesson-leads/index.blade.php
+++ resources/views/private-lesson-leads/index.blade.php
@@ -12,6 +12,12 @@
 @section('content')
     <div class="uk-section-default uk-section uk-section-small">
         <div class="uk-container">
+
+            <div class="uk-alert-primary" uk-alert>
+                <a class="uk-alert-close" uk-close></a>
+                <p>We are no longer taking new requests for private swim lessons at this time as our swim season runs March through October. Please revisit us next year to request private swim lessons starting in February.</p>
+            </div>
+
             <div class="uk-grid-margin uk-grid" uk-grid="">
                 <div class="uk-width-3-4@m uk-first-column">
                     <div class="uk-margin uk-dropcap">
@@ -90,78 +96,78 @@
                     </div>
                 </div>
 
-                <div class="uk-width-1-1@m uk-first-column">
-                    <h2 class="uk-heading-bullet">Request Private Lessons</h2>
-                    <div class="uk-child-width-1-1@m uk-grid-small uk-grid-match" uk-grid>
-                        <div class="uk-card uk-card-default uk-card-body">
-                            <form action="" method="post">
-                                {{ csrf_field() }}
-                                <div class="uk-margin">
-                                    <label class="uk-form-label uk-heading-bullet" for="">Swimmer Name</label>
-                                    <div class="uk-form-controls">
-                                        <input type="text" name="swimmer_name" value="{{ old('swimmer_name') }}" class="uk-input" placeholder="Full Name" required>
-                                    </div>
-                                </div>
-                                <div class="uk-margin">
-                                    <label class="uk-form-label uk-heading-bullet" for="">Swimmer Birth Date</label>
-                                    <div class="uk-form-controls">
-                                        <input type="date" class="uk-input" id="swimmer_birth_date" name="swimmer_birth_date" value="{{ old('swimmer_birth_date') }}" required>
-                                    </div>
-                                </div>
-                                <div class="uk-margin">
-                                    <label class="uk-form-label uk-heading-bullet" for="">Email</label>
-                                    <div class="uk-form-controls">
-                                        <input type="email" name="email" value="{{ old('email') }}" class="uk-input" placeholder="Email@email.com" required>
-                                    </div>
-                                </div>
-                                <div class="uk-margin">
-                                    <label class="uk-form-label uk-heading-bullet" for="">Phone</label>
-                                    <div class="uk-form-controls">
-                                        <input type="tel" name="phone" value="{{ old('phone') }}" class="uk-input" placeholder="999 999-9999" required>
-                                    </div>
-                                </div>
-                                <div class="uk-margin">
-                                    <label class="uk-form-label uk-heading-bullet" for="">Type</label>
-                                    <div class="uk-form-controls">
-                                        <select class="uk-select" name="type" id="type">
-                                            <option value="Private Lesson">Private Lesson (One Swimmer)</option>
-                                            <option value="Semi Private Lesson">Semi Private Lesson (Multiple Swimmers)</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="uk-margin">
-                                    <label class="uk-form-label uk-heading-bullet" for="">Package Option</label>
-                                    <div class="uk-form-controls">
-                                        <select class="uk-select" name="length" id="length">
-                                            <option value="4 Lessons Per Month">4 Lessons Per Month</option>
-                                            <option value="8 Lessons Per Month">8 Lessons Per Month</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="uk-margin">
-                                    <label class="uk-form-label uk-heading-bullet" for="">Location</label>
-                                    <div class="uk-form-controls">
-                                        <select class="uk-select" name="location" id="location">
-                                            <option value="Harrison Ranch">Harrison Ranch</option>
-                                            <option value="My Home or Community Pool">My Home or Community Pool (Provide Address Below)</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="uk-margin">
-                                    <label class="uk-form-label uk-heading-bullet" for="">Provide us with your availability</label>
-                                    <div class="uk-form-controls">
-                                        <textarea name="availability" rows="5" class="uk-textarea" placeholder="Example: I am available Monday from 8am-1pm and Wednesday from 5-9pm." required>{{ old('availability') }}</textarea>
-                                    </div>
-                                </div>
-                                <div uk-grid="" class="uk-grid">
-                                    <div class="uk-width-1-2@s uk-margin">
-                                        <input type="submit" value="Send" class="uk-button-primary uk-button uk-button-large">
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                {{--<div class="uk-width-1-1@m uk-first-column">--}}
+                    {{--<h2 class="uk-heading-bullet">Request Private Lessons</h2>--}}
+                    {{--<div class="uk-child-width-1-1@m uk-grid-small uk-grid-match" uk-grid>--}}
+                        {{--<div class="uk-card uk-card-default uk-card-body">--}}
+                            {{--<form action="" method="post">--}}
+                                {{--{{ csrf_field() }}--}}
+                                {{--<div class="uk-margin">--}}
+                                    {{--<label class="uk-form-label uk-heading-bullet" for="">Swimmer Name</label>--}}
+                                    {{--<div class="uk-form-controls">--}}
+                                        {{--<input type="text" name="swimmer_name" value="{{ old('swimmer_name') }}" class="uk-input" placeholder="Full Name" required>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="uk-margin">--}}
+                                    {{--<label class="uk-form-label uk-heading-bullet" for="">Swimmer Birth Date</label>--}}
+                                    {{--<div class="uk-form-controls">--}}
+                                        {{--<input type="date" class="uk-input" id="swimmer_birth_date" name="swimmer_birth_date" value="{{ old('swimmer_birth_date') }}" required>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="uk-margin">--}}
+                                    {{--<label class="uk-form-label uk-heading-bullet" for="">Email</label>--}}
+                                    {{--<div class="uk-form-controls">--}}
+                                        {{--<input type="email" name="email" value="{{ old('email') }}" class="uk-input" placeholder="Email@email.com" required>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="uk-margin">--}}
+                                    {{--<label class="uk-form-label uk-heading-bullet" for="">Phone</label>--}}
+                                    {{--<div class="uk-form-controls">--}}
+                                        {{--<input type="tel" name="phone" value="{{ old('phone') }}" class="uk-input" placeholder="999 999-9999" required>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="uk-margin">--}}
+                                    {{--<label class="uk-form-label uk-heading-bullet" for="">Type</label>--}}
+                                    {{--<div class="uk-form-controls">--}}
+                                        {{--<select class="uk-select" name="type" id="type">--}}
+                                            {{--<option value="Private Lesson">Private Lesson (One Swimmer)</option>--}}
+                                            {{--<option value="Semi Private Lesson">Semi Private Lesson (Multiple Swimmers)</option>--}}
+                                        {{--</select>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="uk-margin">--}}
+                                    {{--<label class="uk-form-label uk-heading-bullet" for="">Package Option</label>--}}
+                                    {{--<div class="uk-form-controls">--}}
+                                        {{--<select class="uk-select" name="length" id="length">--}}
+                                            {{--<option value="4 Lessons Per Month">4 Lessons Per Month</option>--}}
+                                            {{--<option value="8 Lessons Per Month">8 Lessons Per Month</option>--}}
+                                        {{--</select>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="uk-margin">--}}
+                                    {{--<label class="uk-form-label uk-heading-bullet" for="">Location</label>--}}
+                                    {{--<div class="uk-form-controls">--}}
+                                        {{--<select class="uk-select" name="location" id="location">--}}
+                                            {{--<option value="Harrison Ranch">Harrison Ranch</option>--}}
+                                            {{--<option value="My Home or Community Pool">My Home or Community Pool (Provide Address Below)</option>--}}
+                                        {{--</select>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="uk-margin">--}}
+                                    {{--<label class="uk-form-label uk-heading-bullet" for="">Provide us with your availability</label>--}}
+                                    {{--<div class="uk-form-controls">--}}
+                                        {{--<textarea name="availability" rows="5" class="uk-textarea" placeholder="Example: I am available Monday from 8am-1pm and Wednesday from 5-9pm." required>{{ old('availability') }}</textarea>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div uk-grid="" class="uk-grid">--}}
+                                    {{--<div class="uk-width-1-2@s uk-margin">--}}
+                                        {{--<input type="submit" value="Send" class="uk-button-primary uk-button uk-button-large">--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</form>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
             </div>
         </div>
     </div>
