6d0d7f54d5c6f695db0db2ddae9fffd9e1cb607e
Leping08 <dereks008@gmail.com>
2018-11-15 13:14:17 -0500

    Add nova season filter

---
+++ app/Nova/Filters/Season.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class Season extends Filter
+{
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->where('season_id', $value);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        $result = collect([]);
+
+        foreach (\App\Season::all() as $season){
+            $result->put($season->name(), $season->id);
+        }
+
+        return $result;
+    }
+}
