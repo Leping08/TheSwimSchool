106ec75b66675d079b9e9cb7e72b6151fbe101cd
Leping08 <dereks008@gmail.com>
2018-12-10 13:18:37 -0500

    Refactor PaymentController to use StripeCharge class

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -13,16 +13,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Stripe\Error\InvalidRequest;
+use App\Library\StripeCharge;
 
-//TODO: Refactor this controller with the new stripe class App\Library\StripeCharge
+
 class PaymentController extends Controller
 {
     /**
      * @param Request $request
      * @param $id
+     * @param StripeCharge $stripe
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function ChargeCardForLesson(Request $request, $id)
+    public function ChargeCardForLesson(Request $request, $id, StripeCharge $stripe)
     {
         $request->validate([
             'cardholderName' => 'required',
@@ -34,23 +36,11 @@ class PaymentController extends Controller
         $lesson = Lesson::find($id);
         $swimmer = Swimmer::find($request->swimmerId);
 
-        $this->abortIfLessonIsFull($lesson, $request, $swimmer);
+        $this->abortIfLessonIsFull($lesson, $swimmer);
 
-        try {
-            $charge = $this->chargeCard($lesson, $request, $swimmer);
-        } catch (\Exception $e){
-            Log::error(print_r(request()->server(), true));
-            Log::error("Exception: ".$e->getMessage());
-            Log::error('Something went wrong with the payment sending the user back to the checkout view.');
-            $newSwimmer = $swimmer;
-            return view('swimmers.cardCheckout', compact('lesson', 'newSwimmer'));
-        }
+        $charge = $stripe->pay($request->stripeToken, $lesson->price, $request->cardholderEmail, $lesson->group->type . " swim lessons for $swimmer->firstName $swimmer->lastName through The Swim School.");
 
         $this->updateSwimmerWithPayment($swimmer, $lesson, $charge);
-
-        $this->sendClassSignUpEmail($lesson, $swimmer);
-
-        $this->sendClassFullEmail($lesson);
 
         $request->session()->flash('success', 'Thanks for signing up! The first lesson is '.$lesson->class_start_date->format('F jS').' at '.$lesson->class_start_time->format('g:i a'));
         return redirect('lessons/'.$lesson->class_type);
@@ -60,7 +50,7 @@ class PaymentController extends Controller
      * @param Lesson $lesson
      */
     private function sendClassFullEmail(Lesson $lesson) {
-        if($lesson->isLessonFull()){
+        if($lesson->isFull()){
             try {
                 foreach(config('mail.leadDestEmails') as $email){
                     Log::info("Sending lesson full email to $email. Lesson ID: $lesson->id");
@@ -86,6 +76,7 @@ class PaymentController extends Controller
             Log::error("Email error: ");
             Log::error(print_r($e, true));
         }
+        $this->sendClassFullEmail($lesson);
     }
 
     /**
@@ -94,20 +85,24 @@ class PaymentController extends Controller
      * @param Swimmer $swimmer
      * @return \Illuminate\Http\RedirectResponse
      */
-    private function abortIfLessonIsFull(Lesson $lesson, Request $request, Swimmer $swimmer)
+    private function abortIfLessonIsFull(Lesson $lesson, Swimmer $swimmer)
     {
-        if($lesson->isLessonFull()){
-            $request->session()->flash('danger', 'Sorry the lesson is full.');
+        if($lesson->isFull()){
+            session()->flash('danger', 'Sorry the lesson is full.');
             Log::warning("Swimmer ID: $swimmer->id tried to sign up for a lesson that is full.");
             return back();
         }
     }
 
 
+    /**
+     * @param Swimmer $swimmer
+     * @param Lesson $lesson
+     * @param $charge
+     */
     private function updateSwimmerWithPayment(Swimmer $swimmer, Lesson $lesson, $charge)
     {
         //Mark the as swimmer as payed in the database and save the stripe charge id
-        //TODO if the payment is declined by the bank don't do this step
         $swimmer->stripechargeid = $charge->id;
         $swimmer->paid = 1;
         $swimmer->save();
@@ -115,78 +110,7 @@ class PaymentController extends Controller
 
         $swimmer->update(['lesson_id' => $lesson->id]);
         Log::info("Swimmer: $swimmer->firstName $swimmer->lastName with ID: $swimmer->id signed up for lesson ID: $swimmer->lesson_id");
-    }
 
-
-    private function chargeCard(Lesson $lesson, Request $request, Swimmer $swimmer)
-    {
-        //TODO: Bank declines but they still sign up successfully
-
-        $charge = array(
-            "amount" => $lesson->price * 100,
-            "currency" => "usd",
-            "receipt_email" => "$request->cardholderEmail",
-            "description" => $lesson->group->type . " swim lessons for $swimmer->firstName $swimmer->lastName through The Swim School.",
-            "source" => "$request->stripeToken" //Obtained with Stripe.js
-        );
-
-        Log::info('Stripe charge request array for swimmer ID'. $swimmer->id);
-        Log::info(print_r($charge, true));
-
-        try {
-            \Stripe\Stripe::setApiKey(config('services.stripe.secret'));
-            return \Stripe\Charge::create($charge);
-        } catch (\Stripe\Error\Card $e) {
-            // Since it's a decline, \Stripe\Error\Card will be caught
-            Log::error('Since its a decline, \Stripe\Error\Card will be caught');
-            $this->logStripeError($e);
-            throw new \Exception($e);
-        } catch (\Stripe\Error\RateLimit $e) {
-            // Too many requests made to the API too quickly
-            Log::error('Too many requests made to the API too quickly');
-            $this->logStripeError($e);
-            throw new \Exception($e);
-        } catch (\Stripe\Error\InvalidRequest $e) {
-            // Invalid parameters were supplied to Stripe's API
-            Log::error('Invalid parameters were supplied to Stripes API');
-            $this->logStripeError($e);
-            throw new \Exception($e);
-        } catch (\Stripe\Error\Authentication $e) {
-            // Authentication with Stripe's API failed (maybe you changed API keys recently)
-            Log::error('Authentication with Stripes API failed (maybe you changed API keys recently)');
-            $this->logStripeError($e);
-            throw new \Exception($e);
-        } catch (\Stripe\Error\ApiConnection $e) {
-            // Network communication with Stripe failed
-            Log::error('Network communication with Stripe failed');
-            $this->logStripeError($e);
-            throw new \Exception($e);
-        } catch (\Stripe\Error\Base $e) {
-            // Display a very generic error to the user, and maybe send yourself an email
-            Log::error('Display a very generic error to the user, and maybe send yourself an email');
-            $this->logStripeError($e);
-            throw new \Exception($e);
-        } catch (\Exception $e) {
-            // Something else happened, completely unrelated to Stripe
-            Log::error('Something else happened, completely unrelated to Stripe');
-            Log::error('Error: ' . $e->getMessage());
-            throw new \Exception($e);
-        }
-    }
-
-
-    /**
-     * @param $e
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    private function logStripeError($e)
-    {
-        $body = $e->getJsonBody();
-        $err  = $body['error'];
-        Log::error('Status is:' . $e->getHttpStatus());
-        Log::error('Type is:' . $err['type']);
-        Log::error('Code is:' . $err['code']);
-        Log::error(print_r($err, true));
-        session()->flash('error', 'Oops, something went wrong with the payment. ' . $err['message']);
+        $this->sendClassSignUpEmail($lesson, $swimmer);
     }
 }
