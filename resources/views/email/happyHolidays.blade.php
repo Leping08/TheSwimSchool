5e179990d6f28fda2e5fda6d04c3fb53ca7a378b
Leping08 <dereks008@gmail.com>
2018-12-12 19:53:51 -0500

    Create happy holidays email

---
+++ resources/views/email/happyHolidays.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+
+@component('mail::panel')
+<img src="{{asset('img/hh.jpg')}}">
+@endcomponent
+
+
+@component('mail::panel')
+# What A Year!
+We won the Gold Daisy Award for Best Swim Lessons in Manatee County and started the North River Swim Team this year. Thank you for making our 2018 a great one! We wish you and your family a joyous holiday season and a very happy New Year!
+@endcomponent
+
+Sincerely,<br>
+The Swim School
+@endcomponent
