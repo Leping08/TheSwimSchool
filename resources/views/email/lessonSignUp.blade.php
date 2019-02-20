ac00198593f26426ee97e19e10d6342a1a2ea18b
Leping08 <dereks008@gmail.com>
2019-02-20 13:09:58 -0500

    Update email you get after signing up to use the pool access instructions and add an image

--- resources/views/email/lessonSignUp.blade.php
+++ resources/views/email/lessonSignUp.blade.php
@@ -1,7 +1,10 @@
 @component('mail::message')
 # {{$lesson->group->type}}
 
+<img style="margin-bottom: 2em;" src="{{asset('img/lessons/kids-floating.jpg')}}">
+
 Thanks for signing up for swim lessons through The Swim School.
+
 
 @component('mail::panel')
 ### Time
@@ -9,11 +12,13 @@ Lessons run from {{$lesson->class_start_date->format('l F jS')}} - {{$lesson->cl
 Lessons are on @foreach($lesson->DaysOfTheWeek as $day){{$day->day}}{{$loop->last ? '' : ', '}}@endforeach from {{$lesson->class_start_time->format('g:ia')}} - {{$lesson->class_end_time->format('g:ia')}}.
 @endcomponent
 
+
 @component('mail::panel')
 ### Place
 {{$lesson->location->street}},
 {{$lesson->location->city}}, {{$lesson->location->state}} {{$lesson->location->zip}}
 @endcomponent
+
 
 @component('mail::panel')
 ### What To Bring
@@ -22,10 +27,20 @@ Lessons are on @foreach($lesson->DaysOfTheWeek as $day){{$day->day}}{{$loop->las
 * Sun Screen
 @endcomponent
 
+
+@if($lesson->location->pool_access_instructions)
+@component('mail::panel')
+### Pool Access Instructions
+{{$lesson->location->pool_access_instructions}}
+@endcomponent
+@endif
+
+
 @component('mail::panel')
 ### Inclement Weather
 In the event of inclement weather (i.e. thunder/lightning, heavy rain, and/or air temperature of 65 degrees or below), swim lesson classes will be cancelled. The instructor will contact you via text when a class needs to be cancelled due to inclement weather and will notify you as to when a makeup class will be scheduled. It is best to assume swim lesson classes are on as scheduled if you are not contacted by the instructor. Makeup classes will only be scheduled for swim lesson classes cancelled by the instructor.
 @endcomponent
+
 
 Thanks,<br>
 The Swim School
