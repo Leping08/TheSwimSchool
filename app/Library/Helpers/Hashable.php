5b7b72e02a0fd5ef17eb98b87cd47d56121377cc
Derek Schmitt <dereks008@gmail.com>
2019-04-14 22:42:58 -0400

    Add creating hook to Hashable trait

--- app/Library/Helpers/Hashable.php
+++ app/Library/Helpers/Hashable.php
@@ -7,6 +7,16 @@ namespace App\Library\Helpers;
 trait Hashable
 {
     /**
+     *  This creates the has on the model creating hook
+     */
+    public static function boot()
+    {
+        static::creating(function ($model) {
+            $model->hash = RandomString::generate();
+        });
+    }
+
+    /**
      * @param $query
      * @param $hash
      * @return mixed
