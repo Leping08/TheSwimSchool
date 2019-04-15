06295224b5016569b69b39b332eae361b10d520e
Derek Schmitt <dereks008@gmail.com>
2019-04-14 22:19:10 -0400

    Comment out price on the top of the swim team sign up page

--- resources/views/swim-team/signUp.blade.php
+++ resources/views/swim-team/signUp.blade.php
@@ -13,7 +13,7 @@
                         {{$level->name}} Level Details
                     </div>
                     <div class="uk-child-width-expand@s" uk-grid>
-                        <div><i class="fa fa-money fa-lg" aria-hidden="true"></i> <strong>Price:</strong> ${{$level->price}}</div>
+                        {{--<div><i class="fa fa-money fa-lg" aria-hidden="true"></i> <strong>Price:</strong> ${{$level->price}}</div>--}}
                         <div><i class="fa fa-calendar fa-lg" aria-hidden="true"></i> <strong>Season Length:<br></strong> {{$season->dates}}</div>
                         <div><i class="fa fa-clock-o fa-lg" aria-hidden="true"></i> <strong>Practice Schedule:<br></strong>
                             @foreach($level->schedule as $day)
