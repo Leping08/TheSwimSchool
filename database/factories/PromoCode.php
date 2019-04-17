cbfcfee5ed5b455073c1e917c442a06b1b3ea1b6
Derek Schmitt <dereks008@gmail.com>
2019-04-17 13:13:33 -0400

    Add promo code and swim team level factories

---
+++ database/factories/PromoCode.php
@@ -0,0 +1,10 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\PromoCode::class, function (Faker $faker) {
+    return [
+        'code' => $faker->word,
+        'discount_percent' => $faker->numberBetween(1, 100)
+    ];
+});
