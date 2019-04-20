eac2a276d4af12f9a8877ae770c5fb1ab60901be
Derek Schmitt <dereks008@gmail.com>
2019-04-20 10:21:10 -0400

    Add factory for swim team coach

---
+++ database/factories/STCoach.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\STCoach::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'phone' => '9998887777',
+        'active' => true,
+        'bio' => $faker->paragraph
+    ];
+});
