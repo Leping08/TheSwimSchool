c6e765bfe4b0527d81acf5bd44c4ac139c5eddc2
Derek Schmitt <dereks008@gmail.com>
2019-05-20 13:06:20 -0400

    Update lesson link in registration open email

--- resources/views/email/registrationOpen.blade.php
+++ resources/views/email/registrationOpen.blade.php
@@ -7,7 +7,7 @@
 Registration is now open for our first June weekday session of group swim lessons. Our summer sessions are morning classes four days per week Monday through Thursday. If you are currently enrolled and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
 @endcomponent
 
-@component('mail::button', ['url' => 'https://theswimschoolfl.com/?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
+@component('mail::button', ['url' => 'https://theswimschoolfl.com/lessons?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
     Sign Up
 @endcomponent
 
