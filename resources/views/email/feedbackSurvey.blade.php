21f4a28eac810e885e0305f0d0fde964c7561828
Derek Schmitt <dereks008@gmail.com>
2019-05-29 17:23:44 -0400

    Add feedback survey email

---
+++ resources/views/email/feedbackSurvey.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+
+<img style="margin-bottom: 2em;" src="{{asset('img/lessons/kids-floating.jpg')}}">
+
+# Feedback
+
+Thank you for participating in an aquatics program through The Swim School! To help us serve you better and improve our programs, we need your feedback. Please take a few minutes to complete this evaluation form regarding your recent experience with The Swim School.
+
+@component('mail::button', ['url' => config('app.url').'feedback'])
+Leave Feedback
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
