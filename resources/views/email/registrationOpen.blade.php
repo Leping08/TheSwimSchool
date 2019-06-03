4af5fba60c590a9302e7ce4e9eb739723f3b7342
Derek Schmitt <dereks008@gmail.com>
2019-06-02 20:16:36 -0400

    Fix typo

--- resources/views/email/registrationOpen.blade.php
+++ resources/views/email/registrationOpen.blade.php
@@ -4,7 +4,7 @@
 
 @component('mail::panel')
 # Registration is Now Open
-Registration is now open for our next weekend sessions of group swim lessons. If you are currently enrolled and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
+Registration is now open for our weekend sessions of group swim lessons. If you are currently enrolled and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
 @endcomponent
 
 @component('mail::button', ['url' => 'https://theswimschoolfl.com/lessons?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
