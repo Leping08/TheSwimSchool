f91785a3419e91f7a05a7cbc36f5313d470cb15b
Derek Schmitt <dereks008@gmail.com>
2019-07-21 21:10:47 -0400

    Update email text

--- resources/views/email/registrationOpen.blade.php
+++ resources/views/email/registrationOpen.blade.php
@@ -4,7 +4,7 @@
 
 @component('mail::panel')
 # Registration is Now Open
-Registration is now open for our next weekend sessions of group swim lessons. These and all remaining sessions for the 2019 season will be held at the River Wilderness Golf & Country Club pool. If you are currently enrolled and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
+Registration is now open for our next weekend sessions of group swim lessons. All sessions starting in August and for the remainder of the 2019 season will be held at the River Wilderness Golf & Country Club pool. If you are currently enrolled and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
 @endcomponent
 
 @component('mail::button', ['url' => 'https://theswimschoolfl.com/lessons?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
