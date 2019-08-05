dda43ee39e7bc422d1d50c80bd2ca1e88f0b4636
Derek Schmitt <dereks008@gmail.com>
2019-08-04 21:10:23 -0400

    Add sun image and update marketing email

--- resources/views/email/registrationOpen.blade.php
+++ resources/views/email/registrationOpen.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 
-<img alt="" style="margin-bottom: 2em;" src="{{asset('img/lessons/mom_and_daughter.jpg')}}">
+<img alt="" style="margin-bottom: 2em;" src="{{asset('img/lessons/sun.jpg')}}">
 
 @component('mail::panel')
 # Registration is Now Open
-Registration is now open for our next weekend sessions of group swim lessons. All sessions starting in August and for the remainder of the 2019 season will be held at the River Wilderness Golf & Country Club pool. If you are currently enrolled and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
+Registration is now open for our next weekday morning and evening sessions of group swim lessons. These and all remaining sessions for the 2019 season will be held at the River Wilderness Golf & Country Club pool. If you are currently enrolled and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
 @endcomponent
 
 @component('mail::button', ['url' => 'https://theswimschoolfl.com/lessons?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
