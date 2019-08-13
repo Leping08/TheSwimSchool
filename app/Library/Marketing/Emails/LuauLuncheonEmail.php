8cab3e600da00d2321057db5efd5dbbf835c416f
Derek Schmitt <dereks008@gmail.com>
2019-08-13 13:37:17 -0400

    Add LuauLuncheonEmail class

---
+++ app/Library/Marketing/Emails/LuauLuncheonEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Library\Marketing\Emails;
+
+
+use App\Mail\Awards;
+use App\STSeason;
+use App\STSwimmer;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class LuauLuncheonEmail
+{
+    private $season;
+
+    public function __construct(STSeason $season)
+    {
+        $this->season = $season;
+    }
+
+    public function getEmailAddresses()
+    {
+        return STSwimmer::where('s_t_season_id', $this->season->id)->pluck('email')->unique();
+    }
+
+    public function send()
+    {
+        foreach($this->getEmailAddresses() as $email)
+        {
+            try{
+                Log::info("Going to send swim team luau luncheon email to $email");
+                //Mail::to($email)->send(new Awards());
+            } catch (\Exception $e) {
+                Log::warning("Email error: $e");
+            }
+        }
+    }
+}
