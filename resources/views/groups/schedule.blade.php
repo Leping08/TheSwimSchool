bb99d5d87fef86a15f7d4154bc4f3430e9a010a5
Derek Schmitt <dereks008@gmail.com>
2020-05-02 11:03:20 -0400

    Update group lesson schedule

--- resources/views/groups/schedule.blade.php
+++ resources/views/groups/schedule.blade.php
@@ -13,19 +13,40 @@
     <div class="uk-section-default uk-section-overlap uk-section">
         <div class="uk-container">
             <div>
-                <h2 class="uk-heading-line"><span>Spring 2020 Sessions</span></h2>
+                <h2 class="uk-heading-line"><span>Spring/Summer 2020 Sessions</span></h2>
                 <table class="uk-table uk-table-striped">
                     <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Weekday Sessions (M/W, T/TH 2x/wk for 4 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Weekday Sessions (M/T/W/TH 4x/wk for 2 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>May 4th-May 28th</td>
-                        <td>March 30th</td>
-                    </tr>
+                        <tr>
+                            <td>May 18th-28th</td>
+                            <td>May 11th</td>
+                        </tr>
+                        <tr>
+                            <td>June 1st-June 11th</td>
+                            <td>May 25th</td>
+                        </tr>
+                        <tr>
+                            <td>June 15th-June 25th</td>
+                            <td>June 8th</td>
+                        </tr>
+
+                        <tr>
+                            <td>June 29th-July 9th</td>
+                            <td>June 22nd</td>
+                        </tr>
+                        <tr>
+                            <td>July 13th-July 23rd</td>
+                            <td>July 6th</td>
+                        </tr>
+                        <tr>
+                            <td>July 27th-August 6th</td>
+                            <td>July 20th</td>
+                        </tr>
                     </tbody>
                 </table>
 
@@ -33,16 +54,20 @@
 
                 <table class="uk-table uk-table-striped">
                     <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Saturday Sessions (1x/wk for 6 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Saturday Sessions (1x/wk for 6 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>May 2nd-June 6th</td>
-                        <td>March 30th</td>
-                    </tr>
+                        <tr>
+                            <td>May 23rd-June 27th</td>
+                            <td>May 11th</td>
+                        </tr>
+                        <tr>
+                            <td>July 11th-August 15th</td>
+                            <td>June 29th</td>
+                        </tr>
                     </tbody>
                 </table>
 
@@ -50,118 +75,46 @@
 
                 <table class="uk-table uk-table-striped">
                     <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Sunday Sessions (1x/wk for 6 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Sunday Sessions (1x/wk for 6 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>May 3rd-June 7th</td>
-                        <td>March 30th</td>
-                    </tr>
+                        <tr>
+                            <td>May 24th-June 28th</td>
+                            <td>May 11th</td>
+                        </tr>
+                        <tr>
+                            <td>July 12th-August 16th</td>
+                            <td>June 29th</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
 
-            <div>
-                <h2 class="uk-heading-line"><span>Summer 2020 Sessions</span></h2>
-                <table class="uk-table uk-table-striped">
-                    <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Weekday Sessions (M/T/W/TH 4x/wk for 2 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>June 1st - June 11th</td>
-                        <td>May 18th</td>
-                    </tr>
-                    <tr>
-                        <td>June 15th - June 25th</td>
-                        <td>June 8th</td>
-                    </tr>
-                    <tr>
-                        <td>June 29th - July 9th</td>
-                        <td>June 22nd</td>
-                    </tr>
-                    <tr>
-                        <td>July 13th - July 23rd</td>
-                        <td>July 6th</td>
-                    </tr>
-                    <tr>
-                        <td>July 27th - August 6th</td>
-                        <td>July 20th</td>
-                    </tr>
-                    </tbody>
-                </table>
-
-                <hr>
-
-                <table class="uk-table uk-table-striped">
-                    <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Saturday Sessions (1x/wk for 6 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>June 13th-July 18th</td>
-                        <td>June 1st</td>
-                    </tr>
-                    <tr>
-                        <td>August 1st-September 5t</td>
-                        <td>July 20th</td>
-                    </tr>
-                    </tbody>
-                </table>
-
-                <hr>
-
-                <table class="uk-table uk-table-striped">
-                    <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Sunday Sessions (1x/wk for 6 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>June 14th-July 19th</td>
-                        <td>June 1st</td>
-                    </tr>
-                    <tr>
-                        <td>August 2nd-September 6th</td>
-                        <td>July 20th</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <div>
+            <div class="uk-margin-large-top">
                 <h2 class="uk-heading-line"><span>Fall 2020 Sessions</span></h2>
                 <table class="uk-table uk-table-striped">
                     <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Weekday Sessions (M/W, T/TH 2x/wk for 4 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Weekday Sessions (M/W, T/TH 2x/wk for 4 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>August 10th - September 3rd</td>
-                        <td>August 3rd</td>
-                    </tr>
-                    <tr>
-                        <td>September 7th - October 1st</td>
-                        <td>August 31st</td>
-                    </tr>
-                    <tr>
-                        <td>October 5th - October 29th</td>
-                        <td>September 28th</td>
-                    </tr>
+                        <tr>
+                            <td>August 10th-September 3rd</td>
+                            <td>August 3rd</td>
+                        </tr>
+                        <tr>
+                            <td>September 7th-October 1st</td>
+                            <td>August 31st</td>
+                        </tr>
+                        <tr>
+                            <td>October 5th-October 29th</td>
+                            <td>September 28th</td>
+                        </tr>
                     </tbody>
                 </table>
 
@@ -169,16 +122,16 @@
 
                 <table class="uk-table uk-table-striped">
                     <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Saturday Sessions (1x/wk for 6 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Saturday Sessions (1x/wk for 6 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>September 19th-October 24th</td>
-                        <td>September 7th</td>
-                    </tr>
+                        <tr>
+                            <td>August 29th-October 3rd</td>
+                            <td>August 17th</td>
+                        </tr>
                     </tbody>
                 </table>
 
@@ -186,16 +139,16 @@
 
                 <table class="uk-table uk-table-striped">
                     <thead>
-                    <tr>
-                        <th class="uk-width-1-2"><h5>Sunday Sessions (1x/wk for 6 weeks)</h5></th>
-                        <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
-                    </tr>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Saturday Sessions (1x/wk for 6 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>September 20th-October 25th</td>
-                        <td>September 7th</td>
-                    </tr>
+                        <tr>
+                            <td>August 30th-October 4th</td>
+                            <td>August 17th</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
