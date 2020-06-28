02fee00a8b039d673ccbc42a9bef15c812ced09a
Derek Schmitt <dereks008@gmail.com>
2020-06-28 14:16:33 -0400

    Add dry run for covid update email

---
+++ app/Library/Marketing/Emails/Events/UpdateEmail.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Library;
+
+
+use App\EmailList;
+use App\Mail\NewsLetter\Update;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class UpdateEmail
+{
+    public static function send()
+    {
+        $emails = EmailList::where('subscribe', '=', true)->pluck('email')->all();
+
+        foreach($emails as $email)
+        {
+            try {
+                Log::info("Sending Covid update email to : $email");
+                //Mail::to($email)->send(new Update($email));
+            } catch (\Exception $e) {
+                Log::warning("Email error: $e");
+            }
+        }
+    }
+}
