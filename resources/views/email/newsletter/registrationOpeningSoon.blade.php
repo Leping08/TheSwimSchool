61b62551cbab330827890c902194df8d01efb3e7
Derek Schmitt <dereks008@gmail.com>
2021-03-01 07:39:49 -0500

    Update RegistrationOpeningSoon email

--- resources/views/email/newsletter/registrationOpeningSoon.blade.php
+++ resources/views/email/newsletter/registrationOpeningSoon.blade.php
@@ -1,14 +1,14 @@
 @component('mail::message')
 
-<img style="margin-bottom: 2em;" src="{{asset('img/thank-you/smile.jpg')}}">
+<img style="margin-bottom: 2em;" src="{{asset('img/thank-you/smile.jpg')}}" alt="">
 
 @component('mail::panel')
-# Lesson Registration Opens Feb 11th
-Dust off your goggles and stock up on sunscreen, it’s time to get excited about our upcoming 2019 swim season! The Swim School will be starting programs the first week of March and registration opens online through our website this Monday, February 11th. We are looking forward to seeing you at the pool very soon!
+# 2021 Swim Season Is Almost Here!
+We can’t wait to splash around and make waves at the pool again as soon as the water warms up! Our <a href="{{ route('groups.schedule.index') }}">2021 session schedule</a> is now available to view on the website and registration will open for our first session of group classes, private lessons and spring swim club on Monday, March 15th. We are also very excited to announce that we WILL be able to have a summer swim team season this year! Stay tuned for more swim team details coming in April.
 @endcomponent
 
-@component('mail::button', ['url' => config('app.url').'?utm_source=newsletter&utm_medium=email&utm_campaign=spring_lesson_registration'])
-    Sign Up
+@component('mail::button', ['url' => config('app.url').'?utm_source=newsletter&utm_medium=email&utm_campaign=spring_lesson_registration_coming_soon'])
+    Visit Site
 @endcomponent
 
 Sincerely,<br>
