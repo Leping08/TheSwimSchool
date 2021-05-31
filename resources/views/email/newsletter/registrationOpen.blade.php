8c8c95c867c70d9e82fea4b33308caa012256236
Derek Schmitt <dereks008@gmail.com>
2021-05-31 09:02:52 -0400

    Update marketing email

--- resources/views/email/newsletter/registrationOpen.blade.php
+++ resources/views/email/newsletter/registrationOpen.blade.php
@@ -3,8 +3,8 @@
 <img alt="" style="margin-bottom: 2em;" src="{{asset('img/lessons/hat.jpg')}}">
 
 @component('mail::panel')
-# Weekend Group Swim Lesson Registration Is Now Open!
-Registration is now open for our next weekend session of group swim lessons. As a reminder, all group and private swim lessons are now being held at our new indoor pool location, Realhab Physical Therapy, Aquatics & Wellness Center. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
+# Weekday Group Swim Lesson Registration Is Now Open!
+Registration is now open for our first summer weekday session of group swim lessons. All group swim lesson classes now take place at our new indoor pool location, Realhab Physical Therapy, Aquatics & Wellness Center. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
 @endcomponent
 
 @component('mail::button', ['url' => route('groups.lessons.index').'?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
