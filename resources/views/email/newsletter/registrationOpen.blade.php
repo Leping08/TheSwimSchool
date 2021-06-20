3515c2c34173f3534e9a0310183a5fb6bb4fdf8d
Derek Schmitt <dereks008@gmail.com>
2021-06-20 13:29:54 -0400

    Update email

--- resources/views/email/newsletter/registrationOpen.blade.php
+++ resources/views/email/newsletter/registrationOpen.blade.php
@@ -4,7 +4,7 @@
 
 @component('mail::panel')
 # Weekday Group & Private Lesson Registration Open!
-Registration is now open for our next summer weekday session of group swim lessons as well as some additional private lesson time slots for the month of June with our new instructor, Colleen Allison. All swim lesson classes now take place at our new indoor pool location, Realhab Physical Therapy, Aquatics & Wellness Center. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
+Today is the last day of registration for our next summer weekday session of group swim lessons as well as some additional private lesson time slots for the month of June with our new instructor, Colleen Allison. All swim lesson classes now take place at our new indoor pool location, Realhab Physical Therapy, Aquatics & Wellness Center. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
 @endcomponent
 
 @component('mail::button', ['url' => route('home.index').'?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
