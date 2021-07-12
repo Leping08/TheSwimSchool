a8614dee22cb3e3cb89de243314402d2803276ef
Derek Schmitt <dereks008@gmail.com>
2021-07-12 06:47:54 -0400

    Update email blast

--- resources/views/email/newsletter/registrationOpen.blade.php
+++ resources/views/email/newsletter/registrationOpen.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 
-<img alt="" style="margin-bottom: 2em;" src="{{asset('img/swim-team/backstroke.jpg')}}">
+<img alt="" style="margin-bottom: 2em;" src="{{asset('img/thank-you/bennett.jpgs')}}">
 
 @component('mail::panel')
-# Weekday Group & July Private Lesson Registration Is Now Open!
-Registration is now open for our next weekday session of group swim lessons that starts Monday, July 5th as well as our available private swim lessons for the month of July with our new instructor, <a href="{{route('pages.about') . '#Colleen'}}">Colleen Allison</a>. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
+# Weekday & Weekend Group Lesson Registration Is Now Open!
+Registration is now open for our next weekday and weekend sessions of group swim lessons. We also have a few private swim lesson time slots available for the remainder of July as well. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
 @endcomponent
 
 @component('mail::button', ['url' => route('home.index').'?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
