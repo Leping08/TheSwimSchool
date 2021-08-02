97f626bcc356b38340e99ca23760008463717099
Derek Schmitt <dereks008@gmail.com>
2021-08-02 08:26:04 -0400

    Fix news letter email body

--- resources/views/email/newsletter/registrationOpen.blade.php
+++ resources/views/email/newsletter/registrationOpen.blade.php
@@ -4,7 +4,7 @@
 
 @component('mail::panel')
 # Weekday Group Lesson Registration Is Now Open!
-Registration is now open for our next weekday sessions of group swim lessons as well as our available private swim lesson time slots for the month of August. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.Registration is now open for our next weekday sessions of group swim lessons as well as our available private swim lesson time slots for the month of August. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
+Registration is now open for our next weekday sessions of group swim lessons as well as our available private swim lesson time slots for the month of August. If you are currently enrolled in our group classes and have any questions regarding which level to sign your child up for in the next session, please consult with your instructor.
 @endcomponent
 
 @component('mail::button', ['url' => route('home.index').'?utm_source=newsletter&utm_medium=email&utm_campaign=summer_lesson_registration'])
