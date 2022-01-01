8f2aeb31006a05d82bf9c3d8d95baaa4b6fa9dbe
Leping08 <derek@deltavcreative.com>
2022-01-01 10:33:25 -0500

    Add hour long cache to instructors about page

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Instructor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class AboutController extends Controller
 {
     public function index()
     {
-        $instructors = Instructor::where('active', true)->get();
+        $instructors = Cache::remember('about_instructors', config('cache.time'), function () {
+            return Instructor::where('active', true)->get();
+        });
         return view('pages.about', compact('instructors'));
     }
 }
--- config/cache.php
+++ config/cache.php
@@ -107,4 +107,16 @@ return [
 
     'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | The amount of time a key will stay cached.
+    |
+    */
+
+    'time' => now()->addMinutes(60)
+
 ];
