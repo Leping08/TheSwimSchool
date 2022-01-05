9fadd880b6d6b8206b3870463bb3cf7938d7c6b0
Leping08 <derek@deltavcreative.com>
2022-01-05 07:25:35 -0500

    Add S3 asset function to helpers file

---
+++ app/Http/Helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Function to get an asset from the S3 bucket path.
+ * The project gets assets from both the public path 
+ * which is the default path for the asset helper 
+ * and the aws bucket path which is what this function
+ * does.
+ *
+ * @param string $path
+ * @return string
+ */
+if (! function_exists('aws_asset')) {
+  function aws_asset($path) {
+      return "https://theswimschool-bucket.s3.amazonaws.com/tmp/{$path}";
+  };
+};
