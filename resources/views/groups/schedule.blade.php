9072b45e7594e480fee23c6d24098699c6c49bd8
Derek Schmitt <derek@Dereks-MacBook-Pro-2.local>
2022-07-23 20:05:11 -0400

    Update swim lessons schedule

--- resources/views/groups/schedule.blade.php
+++ resources/views/groups/schedule.blade.php
@@ -6,12 +6,84 @@
 @endsection
 
 @section('heading')
-    Group Lesson Sessions & Registration Schedule
+    Group Swim Lesson Session & Registration Schedule
 @endsection
 
 @section('content')
     <div class="uk-section-default uk-section-overlap uk-section">
         <div class="uk-container">
+            <div>
+                *Class days & times are subject to change per session and level based on facility and instructor availability, so the specific class day & times are not available until registration opens for each session. Registration is completed online through the “Levels” by clicking on the “Find Classes” button.
+            </div>
+
+            <div class="uk-margin-large-top">
+                <h2 class="uk-heading-line"><span>2022 Fall Schedule</span></h2>
+                <table class="uk-table uk-table-striped">
+                    <thead>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Weekday AM & PM Sessions (2x/wk for 4 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>August 22nd-September 15th</td>
+                            <td>August 8th</td>
+                        </tr>
+                        <tr>
+                            <td>September 26th-October 20th</td>
+                            <td>September 19th</td>
+                        </tr>
+                        <tr>
+                            <td>October 24th-November 17th **(No Classes Halloween 10/31; Makeup Fri 11/4)</td>
+                            <td>October 17th</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <hr>
+
+                <table class="uk-table uk-table-striped">
+                    <thead>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Saturday AM Sessions (1x/wk for 6 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>August 20th-October 1st **(No Classes 9/10)</td>
+                            <td>August 8th</td>
+                        </tr>
+                        <tr>
+                            <td>October 8th-November 12th</td>
+                            <td>September 26th</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <hr>
+
+                <table class="uk-table uk-table-striped">
+                    <thead>
+                        <tr>
+                            <th class="uk-width-1-2"><h5>Sunday AM Sessions (1x/wk for 6 weeks)</h5></th>
+                            <th class="uk-width-1-2"><h5>Registration Opens</h5></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>August 21st-October 2nd **(No Classes 9/11)</td>
+                            <td>August 8th</td>
+                        </tr>
+                        <tr>
+                            <td>October 9th-November 13th</td>
+                            <td>September 26th</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
             <div class="uk-margin-large-top">
                 <h2 class="uk-heading-line"><span>2022 Summer Schedule</span></h2>
                 <table class="uk-table uk-table-striped">
@@ -55,10 +127,6 @@
                             <td>July 9th-August 13th</td>
                             <td>June 27th</td>
                         </tr>
-                        <tr>
-                            <td>August 20th-October 1st (No Classes Labor Day Weekend 9/3)</td>
-                            <td>August 8th</td>
-                        </tr>
                     </tbody>
                 </table>
 
@@ -75,10 +143,6 @@
                         <tr>
                             <td>July 10th-August 14th</td>
                             <td>June 27th</td>
-                        </tr>
-                        <tr>
-                            <td>August 21st-October 2nd (No Classes Labor Day Weekend 9/4)</td>
-                            <td>August 8th</td>
                         </tr>
                     </tbody>
                 </table>
@@ -128,11 +192,11 @@
                             <td>January 10th</td>
                         </tr>
                         <tr>
-                            <td>March 26th-May 7th (No Class Easter Weekend 4/16)</td>
+                            <td>March 26th-May 7th **(No Class Easter Weekend 4/16)</td>
                             <td>March 7th</td>
                         </tr>
                         <tr>
-                            <td>May 14th-June 25th (No Class Memorial Day Weekend 5/28)</td>
+                            <td>May 14th-June 25th **(No Class Memorial Day Weekend 5/28)</td>
                             <td>May 2nd</td>
                         </tr>
                     </tbody>
@@ -153,11 +217,11 @@
                             <td>January 10th</td>
                         </tr>
                         <tr>
-                            <td>March 27th-May 8th (No Class Easter Weekend 4/17)</td>
+                            <td>March 27th-May 8th **(No Class Easter Weekend 4/17)</td>
                             <td>March 7th</td>
                         </tr>
                         <tr>
-                            <td>May 15th-June 26th (No Class Memorial Day Weekend 5/29)</td>
+                            <td>May 15th-June 26th **(No Class Memorial Day Weekend 5/29)</td>
                             <td>May 2nd</td>
                         </tr>
                     </tbody>
