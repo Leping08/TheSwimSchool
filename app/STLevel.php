94d0b6d6fd6a6dfc96302624ce68bb68bc04111e
Derek Schmitt <derek@Dereks-MacBook-Pro-2.local>
2023-04-21 21:33:17 -0400

    Add soft deletes to swim team level model

--- app/STLevel.php
+++ app/STLevel.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class STLevel extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
