fbb9f7b75e8aca9f3f1fade591ecf5e72bcd7ce9
Leping08 <dereks008@gmail.com>
2024-05-16 19:53:24 -0400

    Remove body from text message link

--- resources/views/partials/swimmers_sms_link.blade.php
+++ resources/views/partials/swimmers_sms_link.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ 'sms://open?addresses=' . $swimmers_phone_numbers_string . '?&body=Message%20Line%201%E2%80%A8Message%20Line%202' }}" class="link-default">
+<a href="{{ 'sms://open?addresses=' . $swimmers_phone_numbers_string . '?&body=' }}" class="link-default">
     Text Link
 </a>
