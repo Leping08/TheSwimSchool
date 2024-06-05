5cdf894c792b7f7d39fed91bc9a3bd58832cb41f
Leping08 <dereks008@gmail.com>
2024-06-05 07:23:08 -0400

    Update nav links in nova and on the website

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -46,7 +46,7 @@
                                     <li><a href="/admin"><i class="fa fa-th-large fa-lg" aria-hidden="true"></i> Dashboard</a></li>
                                     <li><a href="{{ route('newsletter.index') }}"><i class="fa fa-envelope fa-lg" aria-hidden="true"></i> Email Blast</a></li>
                                     <li><a href="/realhab-attendance"><i class="fa fa-tint fa-lg" aria-hidden="true"></i> Realhab Attendance</a></li>
-                                    {{-- <li><a href="https://datastudio.google.com/open/17dj-YhyTBpc5Rev_w3puKIfPKBALD6oL" target="_blank"><i class="fa fa-area-chart fa-lg" aria-hidden="true"></i> Analytics</a></li> --}}
+                                    <li><a href="/roster"><i class="fa fa-list-ul fa-lg" aria-hidden="true"></i> Swim Team Roster</a></li>
                                     <li><a href="/admin/resources/swimmers"><i class="fa fa-calendar fa-lg" aria-hidden="true"></i> Swimmers</a></li>
                                     <li><a href="/admin/resources/lessons"><i class="fa fa-tint fa-lg" aria-hidden="true"></i> Lessons</a></li>
                                     <li><a href="https://stripe.com/" target="_blank"><i class="fa fa-money fa-lg" aria-hidden="true"></i> Stripe</a></li>
@@ -121,7 +121,7 @@
                                     <li><a href="/admin"><i class="fa fa-th-large fa-lg" aria-hidden="true"></i> Dashboard</a></li>
                                     <li><a href="{{ route('newsletter.index') }}"><i class="fa fa-envelope fa-lg" aria-hidden="true"></i> Email Blast</a></li>
                                     <li><a href="/realhab-attendance"><i class="fa fa-calendar fa-lg" aria-hidden="true"></i> Realhab Attendance</a></li>
-                                    {{-- <li><a href="https://datastudio.google.com/open/17dj-YhyTBpc5Rev_w3puKIfPKBALD6oL" target="_blank"><i class="fa fa-area-chart fa-lg" aria-hidden="true"></i> Analytics</a></li> --}}
+                                    <li><a href="/roster"><i class="fa fa-list-ul fa-lg" aria-hidden="true"></i> Swim Team Roster</a></li>
                                     <li><a href="/admin/resources/swimmers"><i class="fa fa-users fa-lg" aria-hidden="true"></i> Swimmers</a></li>
                                     <li><a href="/admin/resources/lessons"><i class="fa fa-tint fa-lg" aria-hidden="true"></i> Lessons</a></li>
                                     <li><a href="https://stripe.com/" target="_blank"><i class="fa fa-money fa-lg" aria-hidden="true"></i> Stripe</a></li>
