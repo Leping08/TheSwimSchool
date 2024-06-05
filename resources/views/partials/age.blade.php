754e133d2541583320fc7a6a9540fbeb1c9ef0dc
Leping08 <dereks008@gmail.com>
2024-06-04 22:25:48 -0400

    Round year and month down for swimmer age

--- resources/views/partials/age.blade.php
+++ resources/views/partials/age.blade.php
@@ -1,5 +1,5 @@
 @if(\Illuminate\Support\Carbon::parse($birthDate)->diffInYears(\Illuminate\Support\Carbon::now()) < 2)
-    {{\Illuminate\Support\Carbon::parse($birthDate)->diffInMonths(\Illuminate\Support\Carbon::now())}} Months Old
+    {{floor(\Illuminate\Support\Carbon::parse($birthDate)->diffInMonths(\Illuminate\Support\Carbon::now()))}} Months Old
 @else
-    {{\Illuminate\Support\Carbon::parse($birthDate)->diffInYears(\Illuminate\Support\Carbon::now())}} Years Old
+    {{floor(\Illuminate\Support\Carbon::parse($birthDate)->diffInYears(\Illuminate\Support\Carbon::now()))}} Years Old
 @endif
