9de98f268d572eddd432a16c26e836a0021984ae
Leping08 <dereks008@gmail.com>
2024-06-10 18:09:49 -0400

    Add slash to sms link

--- resources/views/partials/swimmers_sms_link.blade.php
+++ resources/views/partials/swimmers_sms_link.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ 'sms:/open?addresses=' . $swimmers_phone_numbers_string }}" class="link-default">
+<a href="{{ 'sms://open?addresses=' . $swimmers_phone_numbers_string }}" class="link-default">
     Text Link
 </a>
